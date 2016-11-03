a272e64595b2492c938935681a86aec49f79ea12
Eric Yohansa <eric.yohansa@outlook.com>
2016-11-03 16:05:46 +0700

    Add roster controller.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const Telegram = require("telegram-node-bot");
 const TelegramBaseController = Telegram.TelegramBaseController;
-const tg = new Telegram.Telegram(process.env.TELEGRAM_BOT_TOKEN);
+let token = process.env.TELEGRAM_BOT_TOKEN;
+const tg = new Telegram.Telegram(token);
 
 class PingController extends TelegramBaseController {
     pingHandler($) {
@@ -16,5 +17,60 @@ class PingController extends TelegramBaseController {
     }
 }
 
+class RosterController extends TelegramBaseController {
+    before(command, scope) {
+        scope.members = [
+            "Aville",
+            "ilLogic",
+            "Vlaicu",
+            "Minerva",
+            "Grimocel"
+        ];
+
+        return scope;
+    }
+
+    rosterHandler($) {
+        switch($.query.game) {
+            case "dota2":
+                $.sendMessage()
+        }
+    }
+
+    get routes() {
+        return {
+            "roster :game": "rosterHandler"
+        }
+    }
+}
+
+class UpdateController extends TelegramBaseController {
+    updateHandler($) {
+        $.sendMessage("Updating myself.");
+    }
+
+    get routes() {
+        return {
+            "update": "updateHandler"
+        }
+    }
+}
+
+class WhoisController extends TelegramBaseController {
+    whoisHandler($) {
+        $.sendMessage("")
+    }
+
+    get routes() {
+        return {
+            "whois :name": "whoisHandler",
+            "whois": ""
+        }
+    }
+}
+
 tg.router
-    .when(["ping"], new PingController());
+    .when(["ping"], new PingController())
+    .when(["/roster", ":game"], new RosterController())
+    .when(["/update"], new UpdateController())
+    .when(["/whois", ":name"], new WhoisController());
